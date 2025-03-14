34993bd9341925ffa5ee41bdacf578a35143b853
L-Andrei <lucas23marin@gmail.com>
2025-03-13 22:01:35 -0300

    Autores: Lucas Andrei e Giovani Zanella

--- source/test.cpp
+++ source/test.cpp
@@ -2,24 +2,35 @@
 #include "Matriz.hpp"
 
 template <typename T>
-Matriz<T> sub(const Matriz<T> &matriz, const Matriz<T> &b) {
-    size_t n = matriz.linhas();
+Matriz<T> sub(Matriz<T> &matriz, Matriz<T> &b)
+{
+    int n = static_cast<int>(matriz.linhas());
     Matriz<T> x(n, 1);
 
-    for (size_t i = n; i >= 1; i--) {
+    for (int i = n - 1; i >= 0; i--) {
         T soma = 0;
-        for (size_t j = i + 1; j <= n && j <= matriz.colunas(); j++) {
-            std::cout << "Acessando matriz(" << i << ", " << j << ") e x(" << j << ", 1)\n";
-            soma += matriz(i, j) * x(j, 1);
+        for (int j = i + 1; j < n; j++) {
+            soma += matriz(i, j) * x(j, 0);
         }
-    
-        if (matriz(i, i) == 0) {
-            throw std::runtime_error("Divisão por zero detectada.");
+        x(i, 0) = (b(i, 0) - soma) / matriz(i, i);
+    }
+
+    return x;
+}
+
+template <typename T>
+Matriz<T> reversub(Matriz<T> &matriz, Matriz<T> &b)
+{
+    int n = static_cast<int>(matriz.linhas());
+    Matriz<T> x(n, 1);
+
+    for (int i = 0; i < n; i++) {
+        T soma = 0;
+        for (int j = 0; j < i; j++) {
+            soma += matriz(i, j) * x(j, 0);
         }
-    
-        std::cout << "Acessando b(" << i << ", 1) e matriz(" << i << ", " << i << ")\n";
-        x(i, 1) = (b(i, 1) - soma) / matriz(i, i);
-    }    
+        x(i, 0) = (b(i, 0) - soma) / matriz(i, i);
+    }
 
     return x;
 }
@@ -30,13 +41,24 @@ int main() {
                                  0,1,-2,
                                  0,0,4};
     Matriz<double> A(3, 3, dados);
+    
+    std::vector<double> dados2 = {2,0,0,
+                                 3,1,0,
+                                 1,-2,4};
+    Matriz<double> B(3, 3, dados2);
 
     // Vetor inicial (2x1)
     std::vector<double> vetor_inicial = {4,2,8};
     Matriz<double> v0(3, 1, vetor_inicial);
 
+    std::vector<double> vetor_inicial2 = {4,2,8};
+    Matriz<double> v1(3, 1, vetor_inicial2);
+
     Matriz<double> v = sub(A,v0);
-    //v.imprimir();
+    v.imprimir();
+
+    Matriz<double> v2 = reversub(B,v1);
+    v2.imprimir();
 
     return 0;
 }
