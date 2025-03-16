06f8ac2665aa3c00a4c2474270640bc55e83f788
Samuel Veras <samuel.bernardo.veras@gmail.com>
2025-03-15 22:03:45 -0300

    Criação do namespace ifnum. Padronização de estilo usando clang-format. Re-reorganização do código.

--- include/AlgebraLinear.hpp
+++ include/AlgebraLinear.hpp
@@ -1,16 +1,17 @@
 #include "Matriz.hpp"
 
+namespace ifnum
+{
+
 template <typename T>
 Matriz<T> sub(Matriz<T> &matriz, Matriz<T> &b)
 {
     int n = static_cast<int>(matriz.linhas());
     Matriz<T> x(n, 1);
 
-    for (int i = n - 1; i >= 0; i--)
-    {
+    for (int i = n - 1; i >= 0; i--) {
         T soma = 0;
-        for (int j = i + 1; j < n; j++)
-        {
+        for (int j = i + 1; j < n; j++) {
             soma += matriz(i, j) * x(j, 0);
         }
         x(i, 0) = (b(i, 0) - soma) / matriz(i, i);
@@ -25,11 +26,9 @@ Matriz<T> reversub(Matriz<T> &matriz, Matriz<T> &b)
     int n = static_cast<int>(matriz.linhas());
     Matriz<T> x(n, 1);
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         T soma = 0;
-        for (int j = 0; j < i; j++)
-        {
+        for (int j = 0; j < i; j++) {
             soma += matriz(i, j) * x(j, 0);
         }
         x(i, 0) = (b(i, 0) - soma) / matriz(i, i);
@@ -37,3 +36,5 @@ Matriz<T> reversub(Matriz<T> &matriz, Matriz<T> &b)
 
     return x;
 }
+
+} // namespace ifnum
